65579caa1a5409729c441262f2ec88cff7818657
develmusa <contact@develmusa.digital>
2023-12-26 20:29:53 +0100

    first commit

---
+++ data/script.js
@@ -0,0 +1,184 @@
+// Get current sensor readings when the page loads  
+window.addEventListener('load', getReadings);
+
+
+// Create Temperature Gauge
+// https://canvas-gauges.com/documentation/user-guide/configuration
+var gaugeRope = new LinearGauge({
+    renderTo: 'gauge-rope',
+    width: 1000,
+    height: 1000,
+    titel: "Rope Tension",
+    units: "m",
+    minValue: 0,
+    maxValue: 300,
+    majorTicks: [
+        "0",
+        "50",
+        "100",
+        "150",
+        "200",
+        "250",
+        "300"
+    ],
+    exactTicks: true,
+    minorTicks: 10,
+    strokeTicks: true,
+    // majorTicksInt: 10,
+    // majorTicksDec: 0,
+    highlightsWidth: 5,
+    numbersMargin: 0,
+    highlights: [
+        {
+            "from": 200,
+            "to": 300,
+            "color": "rgba(255, 0, 0, .75)"
+        }
+    ],
+    colorPlate: "#fff",
+    animationDuration: 100,
+    animationRule: "linear",
+    tickSide: "left",
+    numberSide: "left",
+    needleType: "line",
+    needleSide: "left",
+    needleShadow: false,
+    needleWidth: 10,
+    needleStart: 0,
+    needleEnd: 150,
+    colorNeedle: "rgba(255, 0, 255, 1)",
+    borders: false,
+    borderOuterWidth: 5,
+    borderMiddleWidth: 10,
+    borderInnerWidth: 15,
+    borderShadowWidth: 5,
+    barWidth: 20,
+    barStrokeWidth: 7,
+    barProgress: true,
+    barShadow: 0,
+    barBeginCircle: false,
+    colorValueBoxShadow: false,
+    colorValueBoxBackground: false,
+    value: 180,
+    valueBox: true,
+    valueBoxStroke: 0,
+    valueBoxWidth: 100,
+    valueText: false,
+    valueTextShadow: false,
+    valueBoxBorderRadius: 0,
+    valueInt: 2,
+    valueDec: 0,
+}).draw();
+
+var gaugeSpeed = new LinearGauge({
+    renderTo: 'gauge-speed',
+    width: 1000,
+    height: 1000,
+    units: "km/h",
+    minValue: -30,
+    maxValue: 80,
+    majorTicks: [
+        -30,
+        -20,
+        -10,
+        0,
+        10,
+        20,
+        30,
+        40,
+        50,
+        60,
+        70,
+        80
+    ],
+    exactTicks: true,
+    minorTicks: 5,
+    strokeTicks: true,
+    // majorTicksInt: 10,
+    // majorTicksDec: 0,
+    highlightsWidth: 5,
+    numbersMargin: 0,
+    highlights: [
+        {
+            "from": -30,
+            "to": 0,
+            "color": "rgba(0, 0, 255, .75)"
+        }
+    ],
+    colorPlate: "#fff",
+    animationDuration: 100,
+    animationRule: "linear",
+    tickSide: "left",
+    numberSide: "left",
+    needleType: "line",
+    needleSide: "left",
+    needleShadow: false,
+    needleWidth: 10,
+    needleStart: 0,
+    needleEnd: 150,
+    colorNeedle: "rgba(255, 0, 255, 1)",
+    borders: false,
+    borderOuterWidth: 5,
+    borderMiddleWidth: 10,
+    borderInnerWidth: 15,
+    borderShadowWidth: 5,
+    barWidth: 20,
+    barStrokeWidth: 7,
+    barProgress: true,
+    barShadow: 0,
+    barBeginCircle: false,
+    colorValueBoxShadow: false,
+    colorValueBoxBackground: false,
+    valueBox: true,
+    valueBoxStroke: 0,
+    valueBoxWidth: 100,
+    valueText: false,
+    valueTextShadow: false,
+    valueBoxBorderRadius: 0,
+    valueInt: 2,
+    valueDec: 0,
+    value: 25,
+}).draw();
+
+// Function to get current readings on the webpage when it loads for the first time
+function getReadings(){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
+      console.log(myObj);
+      var rope = myObj.rope;
+      var speed = myObj.speed;
+      gaugeRope.value = rope;
+      gaugeSpeed.value = speed;
+    }
+  }; 
+  xhr.open("GET", "/readings", true);
+  xhr.send();
+}
+
+if (!!window.EventSource) {
+  var source = new EventSource('/events');
+  
+  source.addEventListener('open', function(e) {
+    console.log("Events Connected");
+  }, false);
+
+  source.addEventListener('error', function(e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  }, false);
+  
+  source.addEventListener('message', function(e) {
+    console.log("message", e.data);
+  }, false);
+  
+  source.addEventListener('new_readings', function(e) {
+    console.log("new_readings", e.data);
+    var myObj = JSON.parse(e.data);
+    console.log(myObj);
+    gaugeRope.value = myObj.rope;
+    gaugeSpeed.value = myObj.speed;
+  }, false);
+}
---
+++ src/main.cpp
@@ -0,0 +1,103 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+#include <Arduino_JSON.h>
+
+// Replace with your network credentials
+const char* ssid = "SchränzersHirn";
+const char* password = "88888888";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Create an Event Source on /events
+AsyncEventSource events("/events");
+
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 100;
+
+
+
+// Get Sensor Readings and return JSON object
+String getSensorReadings(){
+  readings["rope"] = random(0, 300);
+  readings["speed"] =  random(-50, 100);
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+// Initialize SPIFFS
+void initSPIFFS() {
+  if (!SPIFFS.begin()) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  Serial.println("SPIFFS mounted successfully");
+}
+
+// Initialize WiFi
+void initWiFiSTA() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void initWiFiAP() {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  Serial.println("Init Sensors....");
+  initWiFiAP();
+  initSPIFFS();
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.serveStatic("/", SPIFFS, "/");
+
+  // Request for the latest sensor readings
+  server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request){
+    String json = getSensorReadings();
+    request->send(200, "application/json", json);
+    json = String();
+  });
+
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    // Send Events to the client with the Sensor Readings Every 10 seconds
+    events.send("ping",NULL,millis());
+    events.send(getSensorReadings().c_str(),"new_readings" ,millis());
+    lastTime = millis();
+  }
+}
